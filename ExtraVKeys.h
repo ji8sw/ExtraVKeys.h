db4441e0eabea68b0d2915d4904e109f59d4f883
James <89285665+ji8sw@users.noreply.github.com>
2025-02-28 05:43:11 +1000

    Create ExtraVKeys.h

---
+++ ExtraVKeys.h
@@ -0,0 +1,66 @@
+#pragma once
+/*
+	List of virtual key defines that remain undefined in C++
+	- Ji9sw
+*/
+
+// Alphanumeric keys
+#define VK_0 0x30
+#define VK_1 0x31
+#define VK_2 0x32
+#define VK_3 0x33
+#define VK_4 0x34
+#define VK_5 0x35
+#define VK_6 0x36
+#define VK_7 0x37
+#define VK_8 0x38
+#define VK_9 0x39
+
+#define VK_A 0x41
+#define VK_B 0x42
+#define VK_C 0x43
+#define VK_D 0x44
+#define VK_E 0x45
+#define VK_F 0x46
+#define VK_G 0x47
+#define VK_H 0x48
+#define VK_I 0x49
+#define VK_J 0x4A
+#define VK_K 0x4B
+#define VK_L 0x4C
+#define VK_M 0x4D
+#define VK_N 0x4E
+#define VK_O 0x4F
+#define VK_P 0x50
+#define VK_Q 0x51
+#define VK_R 0x52
+#define VK_S 0x53
+#define VK_T 0x54
+#define VK_U 0x55
+#define VK_V 0x56
+#define VK_W 0x57
+#define VK_X 0x58
+#define VK_Y 0x59
+#define VK_Z 0x5A
+
+// Punctuation and symbols
+#define VK_SEMICOLON 0xBA  // ';:' key
+#define VK_EQUAL 0xBB       // '=+' key
+#define VK_COMMA 0xBC       // ',<' key
+#define VK_MINUS 0xBD       // '-_' key
+#define VK_PERIOD 0xBE      // '.>' key
+#define VK_SLASH 0xBF       // '/?' key
+#define VK_BACKQUOTE 0xC0   // '`~' key
+
+#define VK_LBRACKET 0xDB    // '[{' key
+#define VK_BACKSLASH 0xDC   // '\|' key
+#define VK_RBRACKET 0xDD    // ']}' key
+#define VK_APOSTROPHE 0xDE  // ''"' key
+
+// Additional control keys
+#define VK_TILDE VK_BACKQUOTE
+#define VK_PLUS VK_EQUAL
+#define VK_UNDERSCORE VK_MINUS
+#define VK_QUESTION VK_SLASH
+#define VK_PIPE VK_BACKSLASH
+#define VK_QUOTE VK_APOSTROPHE
